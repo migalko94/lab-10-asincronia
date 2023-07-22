947e79e9e4671bcbea7faac5dc7de49d5f70d169
migalko94 <miguelgalvezugr@gmail.com>
2023-07-22 21:24:10 +0200

    cambio introduccion personaje

--- src/api.ts
+++ src/api.ts
@@ -15,7 +15,9 @@ export const filtraPersonaje = async (
   personajeIntroducido: string
 ): Promise<Personaje[]> => {
   try {
-    const { data } = await Axios.get(URL_BUSQUEDA + `${personajeIntroducido}`);
+    const { data } = await Axios.get(
+      URL_BUSQUEDA + `${encodeURIComponent(personajeIntroducido)}`
+    );
     return data;
   } catch (error) {
     throw new Error("Error al obtener los personajes filtrados");
